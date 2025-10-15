8c0773b3185134430d4bd32e080752fa4c7061e8
Zephyr <maver1ckfreex@gmail.com>
2025-10-15 04:38:56 +0200

    Update date1x1.py

--- date1x1.py
+++ date1x1.py
@@ -5,6 +5,8 @@ from datetime import date
 def event_recommend():
     t = date.today()
     day=t.day
+    month=t.month
+    users_date=month+0.01*day
     Library_ex=[0,"New Year's Party ","Valentine's Dinner","Carnival Night","Karaoke Night ","Kayaking Trip ","Seaside Picnic","Halloween Party","Thanksgiving Jamboree ","Christmas Dinner","New Year's Party ","Valentine's Dinner","Carnival Night"]
     List_m=[0,1.13,2.14,3.1,4.18,5.5,9.15,10.31,11.26,12.18]
     test_date=8.14
